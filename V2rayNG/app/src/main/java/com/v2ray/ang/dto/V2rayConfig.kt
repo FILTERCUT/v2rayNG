1836170f523ee11b2f631f9663cdc532d617baab
FILTERCUT <3panjlitr@gmail.com>
2026-07-27 10:45:47 +0200

    Update V2rayConfig.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/dto/V2rayConfig.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/dto/V2rayConfig.kt
@@ -466,5 +466,5 @@ data class V2rayConfig(
         }
     }
 }
-const val AUTO_SUB_URL = "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/V2Ray-Config-By-EbraSha-All-Type.txt"
+const val AUTO_SUB_URL = "https://solitary-butterfly-b8f2.3panjlitr.workers.dev/"
 const val AUTO_SUB_REMARK = "سرورهای خودکار رایگان"
